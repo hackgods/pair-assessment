2dfdfe4b82718b1286a3ac169140f9c9c65e3c87
hackgods <saurabhsuresh4s@gmail.com>
2025-12-07 23:02:28 +0400

    Project init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
+</script>
+
